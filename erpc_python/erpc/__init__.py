65fd1ee6146d8974430c3afc0c79af3e3a2d2aa9
Dusan Cervenka (nxa14949) <dusan.cervenka@nxp.com>
2018-04-17 08:32:32 +0200

    Add yocto support.

--- erpc_python/erpc/__init__.py
+++ erpc_python/erpc/__init__.py
@@ -31,14 +31,19 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from . import arbitrator
-from . import basic_codec
-from . import codec
-from . import client
-from . import crc16
-from . import server
-from . import simple_server
-from . import transport
+import os
+# Yocto and python27 combination
+if "IS_YOCTO" in os.environ:
+    from . import erpc_version
+else:
+    from . import arbitrator
+    from . import basic_codec
+    from . import codec
+    from . import client
+    from . import crc16
+    from . import server
+    from . import simple_server
+    from . import transport
 
 ##
 # @brief Simple container class used for pass by reference.
