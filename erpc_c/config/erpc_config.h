7af1ffc5841ac4a2bb6be35da63c1705f4fa30ab
Ibrahim ERTURK <ierturk@ieee.org>
2021-05-02 04:21:49 +0300

    Initial support for Threading/ThreadX and building with CMake

--- erpc_c/config/erpc_config.h
+++ erpc_c/config/erpc_config.h
@@ -29,6 +29,7 @@
 #define ERPC_THREADS_ZEPHYR (3)   //!< ZEPHYR.
 #define ERPC_THREADS_MBED (4)     //!< Mbed OS
 #define ERPC_THREADS_WIN32 (5)    //!< WIN32
+#define ERPC_THREADS_THREADX (6)    //!< THREADX
 
 #define ERPC_NOEXCEPT_DISABLED (0) //!< Disabling noexcept feature.
 #define ERPC_NOEXCEPT_ENABLED (1)  //!<  Enabling noexcept feature.
