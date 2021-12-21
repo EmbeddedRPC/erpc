888979383406f1eb80f30b9b2c6e95670b4b5c54
Cervenka Dusan <cervenka@acrios.com>
2021-12-21 13:16:46 +0100

    Get rid of link_up while loop.

--- erpc_c/transports/erpc_rpmsg_tty_rtos_transport.cpp
+++ erpc_c/transports/erpc_rpmsg_tty_rtos_transport.cpp
@@ -174,9 +174,7 @@ erpc_status_t RPMsgTTYRTOSTransport::init(uint32_t src_addr, uint32_t dst_addr, 
                 ready_cb();
             }
 
-            while (0 == rpmsg_lite_is_link_up(s_rpmsg))
-            {
-            }
+            rpmsg_lite_is_link_up_wait(s_rpmsg);
 
 #if RL_USE_STATIC_API
             m_rpmsg_queue = rpmsg_queue_create(s_rpmsg, m_queue_stack, &m_queue_context);
