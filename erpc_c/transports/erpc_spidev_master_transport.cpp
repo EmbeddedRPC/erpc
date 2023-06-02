781fb03abdb9089f430715e77ddb2be1cdfe2d1a
Dusan Cervenka <cervenka@acrios.com>
2023-06-02 15:02:09 +0200

    Fix spidev init (#363)
    * Fix spidev init
    
    Signed-off-by: Cervenka Dusan <cervenka@acrios.com>
    
    * Fix receive error value for spidev
    
    Signed-off-by: Cervenka Dusan <cervenka@acrios.com>
    
    ---------
    
    Signed-off-by: Cervenka Dusan <cervenka@acrios.com>

--- erpc_c/transports/erpc_spidev_master_transport.cpp
+++ erpc_c/transports/erpc_spidev_master_transport.cpp
@@ -108,7 +108,7 @@ SpidevMasterTransport::~SpidevMasterTransport(void)
 
 erpc_status_t SpidevMasterTransport::init(void)
 {
-    erpc_status_t status;
+    erpc_status_t status = kErpcStatus_Success;
 
     /* Initialize the SPI device */
     /* Open SPI device file descriptor */
@@ -217,7 +217,7 @@ erpc_status_t SpidevMasterTransport::underlyingReceive(uint8_t *data, uint32_t s
 
     if (ERPC_SPIDEV_STATUS_SUCCESS != spidev_transfer(m_spidevHandle, NULL, data, size))
     {
-        status = kErpcStatus_SendFailed;
+        status = kErpcStatus_ReceiveFailed;
     }
 
     return status;
