1d6bde04c4a88f41ab1f01e1fb0450809dfcd2b2
Jan Komarek <jan.komarek@nxp.com>
2023-12-05 15:10:44 +0100

    [RPSDK-1296] Update examples to align with rpmsg-lite

--- examples/zephyr/matrix_multiply_rpmsglite/sysbuild.cmake
+++ examples/zephyr/matrix_multiply_rpmsglite/sysbuild.cmake
@@ -4,7 +4,7 @@
 
 # Add external project
 ExternalZephyrProject_Add(
-    APPLICATION erpc_rpmsg_remote
+    APPLICATION remote_server
     SOURCE_DIR ${APP_DIR}/remote
     BOARD ${SB_CONFIG_RPMSG_LITE_REMOTE_BOARD}
   )
@@ -12,10 +12,10 @@ ExternalZephyrProject_Add(
 # Add dependencies so that the remote sample will be built first
 # This is required because some primary cores need information from the
 # remote core's build, such as the output image's LMA
-add_dependencies(erpc_rpmsg erpc_rpmsg_remote)
-sysbuild_add_dependencies(CONFIGURE erpc_rpmsg erpc_rpmsg_remote)
+add_dependencies(matrix_multiply_rpmsglite remote_server)
+sysbuild_add_dependencies(CONFIGURE matrix_multiply_rpmsglite remote_server)
 
 if(SB_CONFIG_BOOTLOADER_MCUBOOT)
   # Make sure MCUboot is flashed first
-  sysbuild_add_dependencies(FLASH erpc_rpmsg mcuboot)
+  sysbuild_add_dependencies(FLASH matrix_multiply_rpmsglite mcuboot)
 endif()
