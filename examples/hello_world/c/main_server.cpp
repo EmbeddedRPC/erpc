6b1e1bc916b7a5478222d0335df97e1070dc19d5
Jan Komarek <jan.komarek@nxp.com>
2024-02-12 16:20:03 +0100

    Update example
    Add error handler to all C and CPP mains. Fix TCP transport deinitialization. Update CPP example.

--- examples/hello_world/c/main_server.cpp
+++ examples/hello_world/c/main_server.cpp
@@ -1,37 +1,100 @@
-#include "erpc_c/setup/erpc_server_setup.h"
-#include "erpc_c/setup/erpc_transport_setup.h"
-#include "erpc_c/setup/erpc_mbf_setup.h"
-#include "examples/hello_world/shim/c/hello_world_server.hpp"
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "erpc_basic_codec.hpp"
+#include "erpc_crc16.hpp"
+#include "erpc_message_buffer.hpp"
+#include "erpc_simple_server.hpp"
+#include "erpc_tcp_transport.hpp"
+
+#include "hello_world_server.hpp"
 #include "examples/config.h"
-#include <cstdio>
+#include "erpc_error_handler.h"
+
+#include <iostream>
+
+using namespace erpcShim;
+using namespace erpc;
+
+////////////////////////////////////////////////////////////////////////////////
+// Classes
+////////////////////////////////////////////////////////////////////////////////
+
+class MyMessageBufferFactory : public MessageBufferFactory
+{
+public:
+    virtual MessageBuffer create()
+    {
+        uint8_t *buf = new uint8_t[ERPC_DEFAULT_BUFFER_SIZE];
+        return MessageBuffer(buf, ERPC_DEFAULT_BUFFER_SIZE);
+    }
+
+    virtual void dispose(MessageBuffer *buf)
+    {
+        erpc_assert(buf);
+        if (*buf)
+        {
+            delete[] buf->get();
+        }
+    }
+};
 
 class TextService : public TextService_interface
 {
-    /* eRPC call definition */
-    void printText(const char *text) override { printf("%s", text); }
-}
+    void printText(const char *text) { std::cout << text; }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Code
+////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    erpc_transport_t transport;
-    erpc_mbf_t message_buffer_factory;
-    erpc_server_t server;
+    erpc_status_t status;
+
+    /* Init eRPC server components */
+    TCPTransport transport(ERPC_HOSTNAME, ERPC_PORT, true);
+    MyMessageBufferFactory msgFactory;
+    BasicCodecFactory codecFactory;
+    SimpleServer server;
+    Crc16 crc16;
+
+    /* Init service implementation and handler */
     TextService textServiceImpl;
     TextService_service textService(&textServiceImpl);
 
-    /* Init eRPC server infrastructure */
-    transport = erpc_transport_tcp_init(ERPC_HOSTNAME, ERPC_PORT, true);
-    message_buffer_factory = erpc_mbf_dynamic_init();
-    server = erpc_server_init(transport, message_buffer_factory);
+    /* Setup transport */
+    transport.setCrc16(&crc16);
+    status = transport.open();
+
+    if (status != kErpcStatus_Success)
+    {
+        /* print error description */
+        erpc_error_handler(status, 0);
+        return -1;
+    }
+
+    /* Setup server */
+    server.setTransport(&transport);
+    server.setCodecFactory(&codecFactory);
+    server.setMessageBufferFactory(&msgFactory);
 
     /* add custom service implementation to the server */
-    erpc_add_service_to_server(server, &textService);
+    server.addService(&textService);
 
     /* poll for requests */
-    erpc_status_t err = server.poll();
+    status = server.poll();
 
-    /* deinit objects */
-    erpc_server_deinit(server);
-    erpc_mbf_dynamic_deinit(message_buffer_factory);
-    erpc_transport_tcp_deinit(transport);
+    /* handle error status */
+    if (status != kErpcStatus_Success)
+    {
+        /* print error description */
+        erpc_error_handler(status, 0);
+    }
+
+    /* Close transport */
+    transport.close();
 }
