add2f31edbb3954605cea05e01719b4a2ac585ec
Jan Komarek <jan.komarek@nxp.com>
2024-02-23 10:53:15 +0100

    Working basic tests generation (without custom client/server)

--- test/test.cmake
+++ test/test.cmake
@@ -33,13 +33,17 @@ endif()
 
 # Directory where test is generated 
 if (NOT DEFINED TEST_OUT_DIR)
-    set(TEST_OUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/output)
+    set(TEST_OUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
 endif()
 
 # Directory where eRPC shim is generated
+set(ERPC_OUT_ROOT_DIR ${TEST_OUT_DIR})
+
 if (NOT DEFINED ERPC_OUT_DIR)
-    set(ERPC_OUT_DIR ${TEST_OUT_DIR}/erpc_outputs)
+    set(ERPC_OUT_DIR erpc_outputs)
 endif()
+
+set(TEST_ERPC_OUT_DIR ${ERPC_OUT_ROOT_DIR}/${ERPC_OUT_DIR})
 
 # Name of eRPC shim
 if (NOT DEFINED ERPC_NAME)
@@ -50,17 +54,51 @@ if (NOT DEFINED ERPC_NAME_APP)
     set(ERPC_NAME_APP ${ERPC_NAME})
 endif()
 
+if (NOT DEFINED GENERATE_ERPC_IDL_FILES)
+    set(GENERATE_ERPC_IDL_FILES TRUE)
+endif()
+
 # Directory where are Unit Test common files 
 set(UT_COMMON_SRC ${ERPC_TEST_ROOT}/common)
 
 # eRPC IDL file
-set(IDL_FILE ${CURRENT_DIR_NAME}.erpc)
+set(IDL_FILE ${CURRENT_DIR}/${CURRENT_DIR_NAME}.erpc)
 
 message(STATUS ${TEST_NAME})
 
 #######################################################
 
 message(STATUS "Building test: ${TEST_NAME}")
+
+# Add IDL files if not provided
+if (GENERATE_ERPC_IDL_FILES)
+    set(TEST_IDL_FILES
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME_APP}_interface.cpp
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME}_unit_test_common_interface.cpp
+
+        # Client
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME_APP}_client.cpp
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME}_unit_test_common_client.cpp
+
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME_APP}_client.cpp
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME}_unit_test_common_client.cpp
+
+        # Server
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME_APP}_server.cpp
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME}_unit_test_common_server.cpp
+
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME_APP}_server.cpp
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME}_unit_test_common_server.cpp
+    )
+
+    # Generate IDL files
+    add_custom_command(
+        OUTPUT ${TEST_IDL_FILES}
+        COMMAND ${ERPCGEN} -g c -o ${ERPC_OUT_ROOT_DIR} ${IDL_FILE}
+        DEPENDS ${IDL_FILE}
+        COMMENT "Generating IDL files"
+    )
+endif()
 
 # Add sources and includes to client if not provided
 if (NOT TEST_CLIENT_SOURCES)
@@ -69,24 +107,25 @@ if (NOT TEST_CLIENT_SOURCES)
 
         ${TEST_COMMON_DIR}/unit_test_${TRANSPORT}_client.cpp
 
-        ${TEST_OUT_DIR}/${ERPC_NAME_APP}_interface.cpp
-        ${TEST_OUT_DIR}/${ERPC_NAME}_unit_test_common_interface.cpp
-
-        ${TEST_OUT_DIR}/${ERPC_NAME_APP}_client.cpp
-        ${TEST_OUT_DIR}/${ERPC_NAME}_unit_test_common_client.cpp
-
-        ${TEST_OUT_DIR}/c_${ERPC_NAME_APP}_client.cpp
-        ${TEST_OUT_DIR}/c_${ERPC_NAME}_unit_test_common_client.cpp
-
         ${ERPC_ROOT}/erpcgen/src/format_string.cpp
         ${ERPC_ROOT}/erpcgen/src/Logging.cpp
+
+        # IDL files
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME_APP}_interface.cpp
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME}_unit_test_common_interface.cpp
+
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME_APP}_client.cpp
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME}_unit_test_common_client.cpp
+
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME_APP}_client.cpp
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME}_unit_test_common_client.cpp
     )
 endif()
 
 if (NOT TEST_CLIENT_INCLUDES)
     set(TEST_CLIENT_INCLUDES
         ${TEST_OUT_DIR}
-        ${ERPC_OUT_DIR}
+        ${TEST_ERPC_OUT_DIR}
         ${TEST_COMMON_DIR}
         ${TEST_COMMON_DIR}/config
         ${TEST_COMMON_DIR}/gtest
@@ -101,31 +140,30 @@ if (NOT TEST_SERVER_SOURCES)
 
         ${TEST_COMMON_DIR}/unit_test_${TRANSPORT}_server.cpp
 
-        ${TEST_OUT_DIR}/${ERPC_NAME_APP}_interface.cpp
-        ${TEST_OUT_DIR}/${ERPC_NAME}_unit_test_common_interface.cpp
-
-        ${TEST_OUT_DIR}/${ERPC_NAME_APP}_server.cpp
-        ${TEST_OUT_DIR}/${ERPC_NAME}_unit_test_common_server.cpp
-
-        ${TEST_OUT_DIR}/c_${ERPC_NAME_APP}_server.cpp
-        ${TEST_OUT_DIR}/c_${ERPC_NAME}_unit_test_common_server.cpp
-
         ${ERPC_ROOT}/erpcgen/src/format_string.cpp
         ${ERPC_ROOT}/erpcgen/src/Logging.cpp
+
+        # IDL files
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME_APP}_interface.cpp
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME}_unit_test_common_interface.cpp
+
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME_APP}_server.cpp
+        ${TEST_ERPC_OUT_DIR}/${ERPC_NAME}_unit_test_common_server.cpp
+
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME_APP}_server.cpp
+        ${TEST_ERPC_OUT_DIR}/c_${ERPC_NAME}_unit_test_common_server.cpp
     )
 endif()
 
 if (NOT TEST_SERVER_INCLUDES)
     set(TEST_SERVER_INCLUDES
         ${TEST_OUT_DIR}
-        ${ERPC_OUT_DIR}
+        ${TEST_ERPC_OUT_DIR}
         ${TEST_COMMON_DIR}
         ${TEST_COMMON_DIR}/config
         ${ERPC_ROOT}/erpcgen/src
     )
 endif()
-
-generate_shim(${ERPC_OUT_DIR} ${IDL_FILE} ${ERPC_NAME} ${ERPC_OUT_DIR})
 
 message(STATUS "SOURCES: ${ERPC_OUT_DIR}")
 
